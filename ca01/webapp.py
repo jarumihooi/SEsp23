b01cb85d1b64c1cf44e4eef2233823b41be79515
i-bog <iogbogu@brandeis.edu>
2023-03-19 16:14:02 -0400

    adding seasonal fruit prompt

--- ca01/webapp.py
+++ ca01/webapp.py
@@ -35,8 +35,8 @@ def index():
       <ul type="circle">
         <li><a href = "/about">Jeremy Huey</a></li>
         <li><a href = "https://github.com/ctighebrandeis">Chris Tighe</a></li>
-        <li><a href = "https://github.com/i-bog?tab=repositories">Ijeoma Ogbogu</a></li>
-        <li><a href = "/compare">Mengli Yang</a></li>
+        <li><a href = "/seasonalfruit">Ijeoma Ogbogu</a></li>
+        <li><a href = "https://github.com/mengliyang2023">Mengli Yang</a></li>
     </ul>
 
     '''
@@ -115,6 +115,38 @@ def proofhelp():
         '''
 
 
+@app.route('/seasonalfruit', methods=['GET', 'POST'])
+def seasonalfruit():
+    ''' handle a get request by sending a form 
+        and a post request by returning the GPT response
+    '''
+    if request.method == 'POST':
+        prompt = request.form['prompt']
+        print(prompt)
+        # I'll need to add the string append here for prompt engineering
+        prompt = "Please give the best season to eat : " + prompt
+        answer = gptAPI.compare(prompt)
+        return f'''
+        <h1>GPT Demo</h1>
+        <pre style="bgcolor:yellow">{prompt}</pre>
+        <hr>
+        Here is the answer in text mode:
+        <div style="border:thin solid black">{answer}</div>
+        Here is the answer in "pre" mode:
+        <pre style="border:thin solid black">{answer}</pre>
+        <a href={url_for('proofhelp')}> make another query</a>
+        '''
+    else:
+        return '''
+        <h1>GPT Demo App</h1>
+        Enter the name of the fruit to find the best season to eat/buy the fruit.
+        <form method="post">
+            <textarea name="prompt"></textarea>
+            <p><input type=submit value="get response">
+        </form>
+        '''
+
+
 @app.route('/task', methods=['GET', 'TASK'])
 def task():
     '''
@@ -130,36 +162,7 @@ def task():
         Here is the answer in text mode:
 
         '''
-    
-@app.route('/compare', methods=['GET', 'POST'])
-def compare():
-    ''' handle a get request by sending a form 
-        and a post request by returning the GPT response
-    '''
-    if request.method == 'POST':
-        prompt = request.form['prompt']
-        answer = gptAPI.compare()
-        prompt = "What are two numbers you want to compare?" + prompt
-        answer = gptAPI.compare()
-        return f'''
-        <h1>Compare your numbers here!</h1>
-        <pre style="bgcolor:yellow">{prompt}</pre>
-        <hr>
-        Here is the answer in text mode:
-        <div style="border:thin solid black">{answer}</div>
-        Here is the answer in "pre" mode:
-        <pre style="border:thin solid black">{answer}</pre>
-        <a href={url_for('gptdemo')}> make another query</a>
-        '''
-    else:
-        return '''
-        <h1>GPT Demo App</h1>
-        Enter your query below
-        <form method="post">
-            <textarea name="prompt"></textarea>
-            <p><input type=submit value="get response">
-        </form>
-        '''
+
 
 if __name__=='__main__':
     app.run(debug=True,port=5001)
