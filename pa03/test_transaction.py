9d77804f37462bac2157621f909ea29b8653f3a8
i-bog <iogbogu@brandeis.edu>
2023-03-26 22:20:01 -0400

    pytest

--- pa03/test_transaction.py
+++ pa03/test_transaction.py
@@ -17,6 +17,10 @@ cur.execute("SELECT * FROM transactions")
 print(cur.fetchone())
 con.close()
 
+tra.modifyCategory(1, "retail")
+x = tra.showTransactions()
+print(x)
+
 
 
 
@@ -26,9 +30,9 @@ con.close()
 #i  = {'item_num': t[0], 'amount': t[1], 'category text': t[2], 'date': t[3], 'desc': t[4]}
 
 # #(item_num,amount,category,date,description)
-t = {"amount":23, "category":"food","date":'2023-03-26',"desc":"grocceries"}
-t1= {"amount":500, "category":"travel", "date":'2023-05-26',"desc":"hotel"}
-t2= {"amount":6700, "category":"rent", "date":'2023-05-26',"desc":"mortgage"}
+t = {"amount":23, "category":"food","day":5, "month": 12, "year":2023,"desc":"grocceries"}
+t1= {"amount":500, "category":"travel", "day":31, "month": 7, "year":2023,"desc":"hotel"}
+t2= {"amount":6700, "category":"rent", "day":16, "month": 3, "year":2023,"desc":"mortgage"}
 
 x = tra.showTransactions()
 print(x)
@@ -47,13 +51,13 @@ def test_addTransaction():
     cur = con.cursor()
     cur.execute("SELECT * FROM transactions where amount=23")
     x= cur.fetchone()
-    assert x == (23, 'food', '2023-03-26', 'grocceries')
+    assert x == (23, 'food', 5, 12, 2023, 'grocceries')
     cur.execute("SELECT * FROM transactions where amount=500")
     x = cur.fetchone()
-    assert x == (500, 'travel', '2023-05-26', 'hotel')
+    assert x == (500, 'travel', 31, 7, 2023, 'hotel')
     cur.execute("SELECT * FROM transactions where amount=6700")
     x = cur.fetchone()
-    assert x == (6700, 'rent', '2023-05-26', 'mortgage')
+    assert x == (6700, 'rent', 16, 3, 2023, 'mortgage')
     con.close()
 
 
@@ -61,29 +65,35 @@ def test_showTransactions():
     x = tra.showTransactions()
     print(x)
     assert x[0] == {'amount': 23,
-                     'category text': 'food',
-                     'date': '2023-03-26',
-                     'desc': 'grocceries',
-                     'item_num': 1}
+                         'category text': 'food',
+                         'day': 5,
+                         'desc': 'grocceries',
+                         'item_num': 1,
+                        'month': 12,
+                        'year': 2023}
 
     assert x[1] == {'amount': 500,
                     'category text': 'travel',
-                    'date': '2023-05-26',
-                    'desc': 'hotel',
-                    'item_num': 2}
+                    'day': 31,
+                    'desc': 'hotel','item_num': 2,
+                    'month': 7,
+                    'year': 2023}
 
 #index out of bound error
-# def test_sum_by_date():
-#     x = tra.sum_by_date()
+def test_sum_by_day():
+    x = tra.sum_by_day()
+    assert x==[]
 
+# # #index out of bound error
+# def test_sum_by_month():
+#     x = tra.sum_by_month()
+#     assert x == []
+# 
 # #index out of bound error
-# def test_sum_by_week():
-#     x = tra.sum_by_week()
-
-#index out of bound error
 # def test_sum_by_year():
 #     x = tra.sum_by_year()
-#     assert x == "k"
+#     assert x == []
+
 
 
 # def test_sum_by_catergory():
@@ -101,10 +111,12 @@ def test_modifyCategory():
     tra.modifyCategory(1, "retail")
     x = tra.showTransactions()
     assert x[0] ==   {'amount': 23,
-                      'category text': 'retail',
-                      'date': '2023-03-26',
-                       'desc': 'grocceries',
-                       'item_num': 1}
+                     'category text': 'retail',
+                     'day': 5,
+                     'desc': 'grocceries',
+                     'item_num': 1,
+                     'month': 12,
+                     'year': 2023}
 
 def test_delete_transaction():
     x = tra.delete_transaction(row_id=0)
